55b7953039ff96ef985e2560217d158fd92dfd2a
Chris Gullickson <cgullickson5@gmail.com>
2017-12-09 00:33:16 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,9 +22,9 @@ class Song
     @@songs.clear
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  #def to_param
+    #name.downcase.gsub(' ', '-')
+  #end
 
   def self.count
     self.all.count
